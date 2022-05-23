9ae2cb48078727bbf7349c59a00b31d8025356b0
Jhan Carlo Vasquez <jhancarlo.vasquez@tup.edu.ph>
2022-05-23 16:47:11 +0800

    initial commit

---
+++ My Info/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace beginning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string CharacterName = "Jhan Carlo";
+            int CharacterAge;
+            CharacterAge = 20;
+            Console.WriteLine("My name is " + CharacterName);
+            Console.WriteLine("I am " + CharacterAge);
+            Console.WriteLine("I am studying @Polytechnic University of the Philippines Manila");
+            Console.WriteLine("I am pursuing a Bachelor of Science in Computer Engineering.");
+            Console.WriteLine("and it's hard :(((");
+
+            Console.ReadLine();
+        }
+    }
+
+}
